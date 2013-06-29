44c86c2edc1f8789540e5a44933e65c446358c8a
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2013-06-29 14:55:50 +0100

    Add tuple functions for Table
    Ignore-this: e99903d299acb5ec6e5814ab36137466
    
    darcs-hash:20130629135550-04b88-ca899949b8b0eec9a906da4315111867ef03dbd4.gz

--- Karamaan/Opaleye/Table.hs
+++ Karamaan/Opaleye/Table.hs
@@ -5,17 +5,28 @@ import Database.HaskellDB.PrimQuery (PrimQuery(Project, BaseTable),
                                      PrimExpr(AttrExpr),
                                      Attribute, Assoc, times)
 import Control.Arrow ((***))
-import Karamaan.Opaleye.Pack (Pack, pack)
+import Karamaan.Opaleye.Pack (Pack, pack,
+                              unflatten1, flatten1,
+                              unflatten2, flatten2,
+                              unflatten3, flatten3,
+                              unflatten4, flatten4,
+                              unflatten5, flatten5,
+                              unflatten6, flatten6,
+                              unflatten7, flatten7,
+                              unflatten8, flatten8)
 import Karamaan.Opaleye.Wire (Wire, unWire)
 import Karamaan.Opaleye.Aggregate (Writer)
 -- FIXME: don't want to import everything, but we're importing a lot
 -- and I can't be bothered to type it all
-import Karamaan.Opaleye.Aggregators hiding (chain)
+import Karamaan.Opaleye.Aggregators hiding (chain, convert)
 
 data Colspec a = Colspec a (Writer a)
 
 col :: Wire a -> Colspec (Wire a)
 col w = Colspec w (return . unWire)
+
+colspecApp :: (b -> a) -> (a -> b) -> Colspec a -> Colspec b
+colspecApp f g (Colspec a w) = Colspec (g a) (w . f)
 
 colsT1 :: T1 (Colspec a1) -> Colspec (T1 a1)
 colsT1 = id
@@ -54,6 +65,41 @@ colsT8 :: T8 (Colspec a1) (Colspec a2) (Colspec a3) (Colspec a4) (Colspec a5)
           (Colspec a6) (Colspec a7) (Colspec a8)
           -> Colspec (T8 a1 a2 a3 a4 a5 a6 a7 a8)
 colsT8 = chain colsT7
+
+convert :: (b -> a1) -> (a -> b1) -> (a1 -> b) -> (b1 -> Colspec a1)
+          -> a -> Colspec b
+convert u u' f c = colspecApp u f . c . u'
+
+cols1 :: Colspec a -> Colspec a
+cols1 = convert unflatten1 unflatten1 flatten1 colsT1
+
+cols2 :: (Colspec a, Colspec b) -> Colspec (a, b)
+cols2 = convert unflatten2 unflatten2 flatten2 colsT2
+
+cols3 :: (Colspec a, Colspec b, Colspec a3) -> Colspec (a, b, a3)
+cols3 = convert unflatten3 unflatten3 flatten3 colsT3
+
+cols4 :: (Colspec a, Colspec b, Colspec a3, Colspec a4)
+         -> Colspec (a, b, a3, a4)
+cols4 = convert unflatten4 unflatten4 flatten4 colsT4
+
+cols5 :: (Colspec a, Colspec b, Colspec a3, Colspec a4, Colspec a5)
+         -> Colspec (a, b, a3, a4, a5)
+cols5 = convert unflatten5 unflatten5 flatten5 colsT5
+
+cols6 :: (Colspec a, Colspec b, Colspec a3, Colspec a4, Colspec a5, Colspec a6)
+         -> Colspec (a, b, a3, a4, a5, a6)
+cols6 = convert unflatten6 unflatten6 flatten6 colsT6
+
+cols7 :: (Colspec a, Colspec b, Colspec a3, Colspec a4, Colspec a5, Colspec a6,
+          Colspec a7)
+         -> Colspec (a, b, a3, a4, a5, a6, a7)
+cols7 = convert unflatten7 unflatten7 flatten7 colsT7
+
+cols8 :: (Colspec a, Colspec b, Colspec a3, Colspec a4, Colspec a5, Colspec a6,
+          Colspec a7, Colspec a8)
+         -> Colspec (a, b, a3, a4, a5, a6, a7, a8)
+cols8 = convert unflatten8 unflatten8 flatten8 colsT8
 
 makeTable :: Pack a => [String] -> String -> Query a
 makeTable x = makeTable' (zip x x)

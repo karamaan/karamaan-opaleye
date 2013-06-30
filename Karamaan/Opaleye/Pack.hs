38df1d8a2b7f379515d3062438f3e9d6d69edfab
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2013-07-01 00:10:23 +0100

    Pull packMap into Pack class
    Ignore-this: 1a2fe4237b0c0cec2de695d67679a2a0
    
    darcs-hash:20130630231023-04b88-8173a723c2f19ddb8003db648559442a5a2d4519.gz

--- Karamaan/Opaleye/Pack.hs
+++ Karamaan/Opaleye/Pack.hs
@@ -2,19 +2,22 @@
 
 module Karamaan.Opaleye.Pack where
 
-import Control.Arrow ((&&&), first)
+import Control.Arrow ((&&&), first, (***))
 
 import Karamaan.Opaleye.Wire (Wire(Wire), unwire)
 
 class Pack a where
+  packMap :: (String -> String) -> a -> a
   pack'  :: [String] -> (a, [String])
   unpack :: a -> [String]
 
 instance Pack (Wire a) where
+  packMap f = Wire . f . unwire
   pack'  = (Wire . head) &&& tail
   unpack = return . unwire
 
 instance (Pack a, Pack b) => Pack (a, b) where
+  packMap f = packMap f *** packMap f
   pack' s = let (a, rest)  = pack' s
                 (b, rest') = pack' rest
             in ((a, b), rest')
@@ -57,40 +60,41 @@ unflatten10 (a, b, c, a4, a5, a6, a7, a8, a9, a10)
   = (a, (b, (c, (a4, (a5, (a6, (a7, (a8, (a9, a10)))))))))
 
 instance (Pack a, Pack b, Pack c) => Pack (a, b, c) where
+  packMap f = flatten3 . (packMap f *** packMap f) . unflatten3
   pack' = first flatten3 . pack'
   unpack = unpack . unflatten3
 
 instance (Pack a, Pack b, Pack c, Pack d) => Pack (a, b, c, d) where
+  packMap f = flatten4 . (packMap f *** packMap f) . unflatten4
   pack' = first flatten4 . pack'
   unpack = unpack . unflatten4
 
 instance (Pack a, Pack b, Pack c, Pack d, Pack e) => Pack (a, b, c, d, e) where
+  packMap f = flatten5 . (packMap f *** packMap f) . unflatten5
   pack' = first flatten5 . pack'
   unpack = unpack . unflatten5
 
 instance (Pack a, Pack b, Pack c, Pack d, Pack e, Pack f)
          => Pack (a, b, c, d, e, f) where
+  packMap f = flatten6 . (packMap f *** packMap f) . unflatten6
   pack' = first flatten6 . pack'
   unpack = unpack . unflatten6
 
 instance (Pack a, Pack b, Pack c, Pack d, Pack e, Pack f, Pack a7)
          => Pack (a, b, c, d, e, f, a7) where
+  packMap f = flatten7 . (packMap f *** packMap f) . unflatten7
   pack' = first flatten7 . pack'
   unpack = unpack . unflatten7
 
 instance (Pack a, Pack b, Pack c, Pack d, Pack e, Pack f, Pack a7, Pack a8)
          => Pack (a, b, c, d, e, f, a7, a8) where
+  packMap f = flatten8 . (packMap f *** packMap f) . unflatten8
   pack' = first flatten8 . pack'
   unpack = unpack . unflatten8
 
 instance (Pack a, Pack b, Pack c, Pack d, Pack e, Pack f, Pack a7, Pack a8,
           Pack a9, Pack a10)
          => Pack (a, b, c, d, e, f, a7, a8, a9, a10) where
+  packMap f = flatten10 . (packMap f *** packMap f) . unflatten10
   pack' = first flatten10 . pack'
   unpack = unpack . unflatten10
-
-pack :: Pack a => [String] -> a
-pack = fst . pack'
-
-packMap :: Pack a => (String -> String) -> a -> a
-packMap f = pack . map f . unpack

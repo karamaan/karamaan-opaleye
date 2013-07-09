3a0463b610ba988750ecb38f589b1309fabb3a23
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2013-07-09 10:22:37 +0100

    Add unary operators creating stuff
    Ignore-this: e744c8fd12fa35283ebd179ea085fb29
    
    darcs-hash:20130709092237-04b88-632ad89b01c8dc8a27228312c5b707017f65f87f.gz

--- Karamaan/Opaleye/Operators.hs
+++ Karamaan/Opaleye/Operators.hs
@@ -1,10 +1,18 @@
 module Karamaan.Opaleye.Operators where
 
 import Database.HaskellDB.Query (ShowConstant, showConstant)
-import Database.HaskellDB.PrimQuery (PrimExpr(BinExpr,
+import Database.HaskellDB.PrimQuery (PrimExpr(BinExpr, UnExpr,
                                               ConstExpr),
-                                     BinOp, Assoc)
+                                     BinOp, UnOp, Assoc)
 import Karamaan.Opaleye.Wire (Wire(Wire))
+
+unOp :: UnOp -> String -> PrimExpr -> String
+          -> (String -> String)
+          -> (Assoc, Wire a)
+unOp op opname t t_string tag' =
+  let newWireName = (tag' . concat) [opname, "_", t_string]
+      newAssoc = [(newWireName, UnExpr op t)]
+  in (newAssoc, Wire newWireName)
 
 binOp' :: BinOp -> String -> PrimExpr -> String -> PrimExpr -> String
           -> (String -> String)
--- Karamaan/Opaleye/Operators2.hs
+++ Karamaan/Opaleye/Operators2.hs
@@ -10,13 +10,16 @@ import Database.HaskellDB.PrimQuery (PrimQuery(Project, Binary,
                                      PrimExpr(AttrExpr, ConstExpr),
                                      BinOp(OpPlus, OpDiv, OpMul, OpOther,
                                            OpMinus),
+                                     UnOp(OpIsNull),
                                      Assoc,
                                      Literal(OtherLit))
 import qualified Database.HaskellDB.PrimQuery as PrimQuery
 import Karamaan.Opaleye.Operators (binOp')
 import qualified Karamaan.Opaleye.Operators as Operators
 import Karamaan.Opaleye.Pack (Pack, packMap, unpack)
-import Control.Arrow ((***))
+import Control.Arrow ((***), arr, (<<<), second)
+import Data.Time.Calendar (Day)
+import Karamaan.WhaleUtil.Date (dayToSQL)
 
 unOp :: ShowConstant c => BinOp -> String -> String -> c
         -> QueryArr (Wire a) (Wire a)
@@ -71,10 +74,19 @@ notEq = opArr PrimQuery.OpNotEq "not_eq"
 cat :: QueryArr (Wire String, Wire String) (Wire String)
 cat = opArr (PrimQuery.OpOther "||") "cat"
 
+isNull :: QueryArr (Wire (Maybe a)) (Wire Bool)
+isNull = unOpArr OpIsNull "is_null"
+
 opArr :: BinOp -> String -> QueryArr (Wire a, Wire a) (Wire b)
 opArr op opname = QueryArr f
   where f ((u, u'), primQ, t1) = (newWire, extend newAssoc primQ, next t1)
           where (newAssoc, newWire) = wireBinOp op opname u u' t1
+
+-- TODO: duplication with opArr?
+unOpArr :: UnOp -> String -> QueryArr (Wire a) (Wire b)
+unOpArr op opname = QueryArr f
+  where f (u, primQ, t1) = (newWire, extend newAssoc primQ, next t1)
+          where (newAssoc, newWire) = wireUnOp op opname u t1
 
 -- FIXME: what's the right type signature for this?
 -- TODO: there's some duplication between this, binOp' and wireOp
@@ -83,6 +95,12 @@ wireBinOp op opname u u' t1 = binOp' op opname (AttrExpr w) w (AttrExpr w') w'
                                      (tagWith t1)
   where w = unWire u
         w' = unWire u'
+
+-- TODO: some duplication with wireBinOp?
+wireUnOp :: UnOp -> String -> Wire a -> Tag -> (Assoc, Wire a2)
+wireUnOp op opname u t1 = Operators.unOp op opname (AttrExpr w) w
+                                         (tagWith t1)
+  where w = unWire u
 
 constantLit :: Literal -> Query (Wire a)
 constantLit l = QueryArr f where

f56b5b0b03bf49a8fed190901f0c746a700e840e
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2013-07-18 16:13:57 +0100

    Truncate argument column names before pasting them together in binOp'
    Ignore-this: 9b0c6c205c5c6b47ebffeaf0511bf327
    Postgres was complaining that column names were getting too long and it was
    truncating them.
    
    darcs-hash:20130718151357-04b88-a4d8e071e817790d5fddf65ad48426248e7d30d0.gz

--- Karamaan/Opaleye/Operators.hs
+++ Karamaan/Opaleye/Operators.hs
@@ -18,7 +18,12 @@ binOp' :: BinOp -> String -> PrimExpr -> String -> PrimExpr -> String
           -> (String -> String)
           -> (Assoc, Wire a)
 binOp' op opname t t_string t' t'_string tag' =
-  let newWireName = (tag' . concat) [t_string, "_", opname, "_", t'_string]
+  -- TODO vv I put this take 5 in here because the query strings were getting
+  -- too long and postgres was complaining that it was truncating them.
+  -- This is really just a temporary fix, because I'd like to keep the
+  -- possibility of long names but postprocess the PrimQuery to shorten
+  -- them before sending them to postgres.
+  let newWireName = (tag' . concat) [take 5 t_string, "_", opname, "_", take 5 t'_string]
       newAssoc = [(newWireName, BinExpr op t t')]
   in (newAssoc, Wire newWireName)
 

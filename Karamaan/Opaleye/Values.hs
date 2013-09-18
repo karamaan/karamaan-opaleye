b6ee2a469ff2870d077101593850c6662f141020
Tom Ellis <tom-2013@purelyagile.com>
2013-09-18 17:55:23 +0100

    Support zero row anonymous tables in Values.hs

--- Karamaan/Opaleye/Values.hs
+++ Karamaan/Opaleye/Values.hs
@@ -1,11 +1,12 @@
 module Karamaan.Opaleye.Values where
 
+import Prelude hiding (Integer)
 import Karamaan.Opaleye.QueryArr (Query)
 import Karamaan.Opaleye.Colspec (col)
 import Karamaan.Opaleye.Table (makeTable)
 import Data.List (intercalate)
 import Karamaan.Opaleye.Wire (Wire)
-import Control.Arrow ((***))
+import Control.Arrow ((***), first)
 import Control.Monad.State (State, get, put, runState)
 import Control.Monad.Reader (ReaderT, runReaderT, ask)
 import Karamaan.Opaleye.Colspec (Colspec, colsT2)
@@ -16,7 +17,23 @@ import Control.Applicative (liftA2)
 
 type S a = ReaderT String (State Int) a
 
-data ValuesMaker a b = ValuesMaker (a -> [String], S (Colspec b))
+data SQLType = Integer | Text | Date | Boolean deriving Show
+
+showSQLType :: SQLType -> String
+-- vv Just using show works currently, but there's no reason
+--    it will for all the types we want to represent, so
+--    consider this function as a "hack for now".
+showSQLType = show
+
+-- TODO: don't know why these are in a tuple!
+-- We would like to enforce the condition that ValuesMaker (f, c) ts
+-- has the image of f with constant length, which equals the length of
+-- ts, and also matches c in length somehow too I guess.  Not sure how to
+-- arrange that.
+-- Probably should make a type for it, in fact.
+-- data ConstLengthListMap a = C (a -> [String])
+-- and then only provide operators that preserve that condition.
+data ValuesMaker a b = ValuesMaker (a -> [String], S (Colspec b), [SQLType])
 
 -- If and when we make Colspec a profunctor I guess we could make
 -- ValuesMaker a profunctor
@@ -24,9 +41,11 @@ data ValuesMaker a b = ValuesMaker (a -> [String], S (Colspec b))
 --bimap f g (ValuesMaker p q) = ValuesMaker (fmap (. f) p) (fmap g q)
 
 (****) :: ValuesMaker a b -> ValuesMaker a' b' -> ValuesMaker (a, a') (b, b')
-(****) (ValuesMaker (f, m)) (ValuesMaker (f', m')) = ValuesMaker (f'', m'')
+(****) (ValuesMaker (f, m, ts)) (ValuesMaker (f', m', ts'))
+  = ValuesMaker (f'', m'', ts'')
   where f'' = catResults f f'
         m'' = liftA2 (curry colsT2) m m'
+        ts'' = ts ++ ts'
 
 (.:.) :: (r -> z) -> (a -> b -> c -> r) -> (a -> b -> c -> z)
 (f .:. g) x y z = f (g x y z)
@@ -41,19 +60,19 @@ nextColName :: S String
 nextColName = do { s <- ask; a <- nextCol; return (s ++ show a) }
 
 string :: ValuesMaker String (Wire String)
-string = valuesMakerMaker singleEnquoten
+string = valuesMakerMaker singleEnquoten Text
 
 int :: ValuesMaker Int (Wire Int)
-int = valuesMakerMaker show
+int = valuesMakerMaker show Integer
 
 day :: ValuesMaker Day (Wire Day)
-day = valuesMakerMaker dayToSQL
+day = valuesMakerMaker dayToSQL Date
 
 bool :: ValuesMaker Bool (Wire Bool)
-bool = valuesMakerMaker show
+bool = valuesMakerMaker show Boolean
 
-valuesMakerMaker :: (a -> String) -> ValuesMaker a (Wire b)
-valuesMakerMaker f = ValuesMaker ((:[]) . f, w)
+valuesMakerMaker :: (a -> String) -> SQLType -> ValuesMaker a (Wire b)
+valuesMakerMaker f t = ValuesMaker ((:[]) . f, w, [t])
   where w = do { n <- nextColName; return (col n) }
 
 -- TODO: this doesn't belong here
@@ -65,8 +84,9 @@ dayToSQL = (++ " :: date") . singleEnquoten . UD.dayToSQL
 --unit = ValuesMaker (return (const [])) (return colsT0)
 
 runValuesMaker :: ValuesMaker a b -> String -> [a]
-                  -> ([[String]], Colspec b, Int)
-runValuesMaker (ValuesMaker (f, m)) colPrefix a = (stringRows, colspec,nextCol')
+                  -> ([[String]], Colspec b, Int, [SQLType])
+runValuesMaker (ValuesMaker (f, m, ts)) colPrefix a
+   = (stringRows, colspec, nextCol', ts)
   where startColNum = 1
         mapper = f
         (colspec, nextCol') = runS m colPrefix startColNum
@@ -75,15 +95,32 @@ runValuesMaker (ValuesMaker (f, m)) colPrefix a = (stringRows, colspec,nextCol')
 runS :: S a -> String -> Int -> (a, Int)
 runS m c s = runState (runReaderT m c) s
 
-valuesToQuery' :: ([[String]], Colspec b, Int) -> Query b
-valuesToQuery' (stringRows, colspec, nextCol') = makeTable colspec select
-  where columnSelectors = map (colNames . show) [1..nextCol'-1]
+-- I guess we'll have a bug if there are no columns at all, but it doesn't seem
+-- like we can create a zero column ValuesMaker without the constructor, so
+-- that's nice.
+-- We *had* a bug where we couldn't create tables with no rows, but I fixed
+-- that with a hack.  It requires the Postgres type information to be passed
+-- around unfortunately, too, because the trick requires we use NULLs, and
+-- Postgres doesn't have polymorphism (at least the right kind of polymorphism).
+valuesToQuery' :: ([[String]], Colspec b, Int, [SQLType]) -> Query b
+valuesToQuery' (stringRows, colspec, nextCol', ts) = makeTable colspec select
+  where colNumbers = map show [1..nextCol'-1]
+        columnSelectors = map colNames colNumbers
         colNames x = "column" ++ x ++ " as foocol" ++ x
         select = (embracket . intercalate " ") [ "select"
                                                , intercalate "," columnSelectors
                                                , "from"
-                                               , valuesOfStringRows stringRows
-                                               , "as foo" ]
+                                               , values
+                                               , "as foo"
+                                               , maybeWhere ]
+        whenSome = (stringRows, "")
+        whenNone = ([map nullOfType ts], "where false")
+
+        (values, maybeWhere) = first valuesOfStringRows $ if null stringRows
+                                                          then whenNone
+                                                          else whenSome
+        typeSig x y = x ++ " :: " ++ showSQLType y
+        nullOfType = typeSig "NULL"
 
 valueOfStringRow :: [String] -> String
 valueOfStringRow = embracket . intercalate ","

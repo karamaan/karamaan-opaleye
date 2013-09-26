9c3c3184980a03f519750aad272b4f5b212c5152
Tom Ellis <tom-2013@purelyagile.com>
2013-09-26 13:50:08 +0100

    Move bendFst and bendSnd to Opaleye

--- Karamaan/Opaleye/Bend.hs
+++ Karamaan/Opaleye/Bend.hs
@@ -2,11 +2,12 @@
 
 module Karamaan.Opaleye.Bend where
 
-import Karamaan.Opaleye.QueryArr (QueryArr)
+import Karamaan.Opaleye.QueryArr (Query, QueryArr)
 import Control.Arrow ((<<<), arr, returnA)
 import Karamaan.Opaleye.Wire (Wire)
 import Karamaan.Opaleye.Operators2 (eq)
 import Karamaan.Opaleye.Predicates (restrict)
+import qualified Karamaan.WhaleUtil.Arrow as UA
 
 bend :: (b -> Wire x) -> QueryArr a b -> QueryArr (Wire x, a) b
 bend f q = proc (x, rest) -> do
@@ -14,3 +15,9 @@ bend f q = proc (x, rest) -> do
   x' <- arr f -< b
   restrict <<< eq -< (x, x')
   returnA -< b
+
+bendFst :: Query (Wire a, Wire b) -> QueryArr (Wire a) (Wire b)
+bendFst q = arr snd <<< bend fst q <<< UA.removeUnit
+
+bendSnd :: Query (Wire a, Wire b) -> QueryArr (Wire b) (Wire a)
+bendSnd q = arr fst <<< bend snd q <<< UA.removeUnit

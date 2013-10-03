c1e3a3f5c5a12fb7897267a144b8e2145c8f02f5
Tom Ellis <tom-2013@purelyagile.com>
2013-10-03 21:15:59 +0100

    QueryRunner, RunQuery and reflection stuff

--- Karamaan/Opaleye/SQL.hs
+++ Karamaan/Opaleye/SQL.hs
@@ -5,9 +5,16 @@ import Database.HaskellDB.Sql.Generate (sqlQuery)
 import Database.HaskellDB.Sql.Print (ppSql)
 import Database.HaskellDB.Sql.Default (defaultSqlGenerator)
 import Database.HaskellDB.PrimQuery (PrimQuery)
-import Karamaan.Opaleye.QueryArr (Query, runQueryArrPrim)
+import Karamaan.Opaleye.QueryArr (Query, runQueryArrPrim, runQueryArrPrim')
 import Karamaan.Opaleye.Pack (Pack)
+import Karamaan.Opaleye.Unpackspec (Unpackspec)
+import Karamaan.WhaleUtil ((.:))
 
+showSqlForPostgreSQLSimple' :: Unpackspec a -> Query a -> String
+showSqlForPostgreSQLSimple' = optimizeFormatAndShowSQL .: runQueryArrPrim'
+
+-- FIXME: vv Once RunQuery is done, this one should go,
+-- leaving only the one above
 showSqlForPostgreSQLSimple :: Pack a => Query a -> String
 showSqlForPostgreSQLSimple = optimizeFormatAndShowSQL . runQueryArrPrim
 

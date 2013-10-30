abe8deeccd42114a852a32842615140ab4c5bc72
Tom Ellis <tom-2013@purelyagile.com>
2013-10-30 18:57:04 +0000

    Comment about module dependency structure

--- Karamaan/Opaleye/Wire.hs
+++ Karamaan/Opaleye/Wire.hs
@@ -9,7 +9,10 @@ unWire (Wire a) = a
 unwire :: Wire a -> String
 unwire = unWire
 
--- TODO: should this always be within a Query?
+-- TODO: should this always be within a QueryArr?
 -- If so, replace Join.unsafeCoerce with it
+--
+-- This would introduce a dependency on QueryArr, so we have
+-- to think carefully about the module dependency structure.
 unsafeCoerce :: Wire a -> Wire b
 unsafeCoerce = Wire . unWire

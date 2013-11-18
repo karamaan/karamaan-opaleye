2193f5dbe74326d7312bf22e155ae22d2118713f
Tom Ellis <tom-2013@purelyagile.com>
2013-11-18 11:57:32 -0500

    Deprecate unwire

--- Karamaan/Opaleye/Wire.hs
+++ Karamaan/Opaleye/Wire.hs
@@ -5,7 +5,8 @@ newtype Wire a = Wire String deriving Show
 unWire :: Wire a -> String
 unWire (Wire a) = a
 
--- TODO: get rid of this one
+-- TODO get rid of this soon
+{-# DEPRECATED unwire "Use 'unWire' instead" #-}
 unwire :: Wire a -> String
 unwire = unWire
 

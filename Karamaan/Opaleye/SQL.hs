cfe79a82182ff187b08c59f1fd1f7db05f9d3b82
Tom Ellis <tom-2013@purelyagile.com>
2013-11-20 16:53:09 -0500

    Add showSqlForPostgresDefault

--- Karamaan/Opaleye/SQL.hs
+++ Karamaan/Opaleye/SQL.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FlexibleContexts,
+    ScopedTypeVariables #-}
+-- TODO: ^^ get rid of these extensions when we move the default instance
+-- somewhere more sensible
+
 module Karamaan.Opaleye.SQL where
 
 import Database.HaskellDB.Optimize (optimize)
@@ -6,8 +11,12 @@ import Database.HaskellDB.Sql.Print (ppSql)
 import Database.HaskellDB.Sql.Default (defaultSqlGenerator)
 import Database.HaskellDB.PrimQuery (PrimQuery)
 import Karamaan.Opaleye.QueryArr (Query, runQueryArrPrim)
-import Karamaan.Opaleye.Unpackspec (Unpackspec)
+import Karamaan.Opaleye.Unpackspec (Unpackspec(Unpackspec))
+import Karamaan.Opaleye.QueryColspec (writerWire)
 import Karamaan.WhaleUtil ((.:))
+import Karamaan.Opaleye.Default (Default, def)
+import Data.Profunctor.Product (PPOfContravariant(PPOfContravariant))
+import Karamaan.Opaleye.Wire (Wire)
 
 {-# DEPRECATED showSqlForPostgreSQLSimple'
     "Use 'showSqlForPostgres' instead" #-}
@@ -32,3 +41,13 @@ formatAndShowSQL = show . ppSql . sqlQuery defaultSqlGenerator
 
 optimizeFormatAndShowSQL :: PrimQuery -> String
 optimizeFormatAndShowSQL = formatAndShowSQL . optimize
+
+instance Default (PPOfContravariant Unpackspec) (Wire a) (Wire a) where
+  def = PPOfContravariant (Unpackspec writerWire)
+
+showSqlForPostgresDefault :: forall a.
+                             Default (PPOfContravariant Unpackspec) a a
+                             => Query a
+                             -> String
+showSqlForPostgresDefault = showSqlForPostgres def'
+  where PPOfContravariant def' = def :: PPOfContravariant Unpackspec a a

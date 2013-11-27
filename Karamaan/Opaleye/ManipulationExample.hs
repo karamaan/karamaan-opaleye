1735b434cc90b997136533efcedf969f4371b15f
Tom Ellis <tom-2013@purelyagile.com>
2013-11-27 16:28:01 +0000

    Use arrow notation in examples

--- Karamaan/Opaleye/ManipulationExample.hs
+++ Karamaan/Opaleye/ManipulationExample.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE Arrows #-}
+
 module Karamaan.Opaleye.ManipulationExample where
 
 import Prelude hiding (or)
@@ -5,43 +7,50 @@ import Karamaan.Opaleye.Wire (Wire(Wire))
 import Karamaan.Opaleye.ExprArr (ExprArr, Expr, eq, plus, constant, or)
 import Data.Profunctor.Product (unPPOfContravariant)
 import Database.HaskellDB.Sql.Print (ppDelete, ppInsert, ppUpdate)
-import Control.Arrow ((&&&), (<<<), first, arr)
+import Control.Arrow (returnA)
 import Karamaan.Opaleye.Default (Default, def)
 import Karamaan.Opaleye.Manipulation (Table(Table), arrangeDelete,
                                       arrangeInsert, arrangeUpdate)
 
+table :: Table ((Wire Int, Wire Int), Wire Int)
+table = Table "tablename" ((Wire "col1", Wire "col2"), Wire "col3")
+
 testDelete :: String
 testDelete = show (ppDelete sqlDelete')
-  where table :: Table ((Wire Int, Wire Int), Wire Int)
-        table = Table "tablename" ((Wire "col1", Wire "col2"), Wire "col3")
-        condExpr :: ExprArr ((Wire Int, Wire Int), Wire Int) (Wire Bool)
-        condExpr = or <<< ((eq <<< first plus) &&& (eq <<< arr (fst . fst) &&& arr snd))
+  where condExpr :: ExprArr ((Wire Int, Wire Int), Wire Int) (Wire Bool)
+        condExpr = proc ((x, y), z) -> do
+          x_plus_y <- plus -< (x, y)
+          cond1 <- eq -< (x_plus_y, z)
+          cond2 <- eq -< (x, z)
+          or -< (cond1, cond2)
         sqlDelete' = arrangeDelete def table condExpr
 
 testInsert :: String
 testInsert = show (ppInsert sqlInsert')
-  where table :: Table ((Wire Int, Wire Int), Wire Int)
-        table  = Table "tablename" ((Wire "col1", Wire "col2"), Wire "col3")
-        insertExpr :: Expr ((Maybe (Wire Int), Maybe (Wire Int)),
+  where insertExpr :: Expr ((Maybe (Wire Int), Maybe (Wire Int)),
                             Maybe (Wire Int))
-        insertExpr = ((arr Just <<< constant 1)
-                      &&& (arr (const Nothing)))
-                     &&& (arr Just <<< plus <<< (constant 5 &&& constant 6))
+        insertExpr = proc () -> do
+          one <- constant 1 -< ()
+          five <- constant 5 -< ()
+          six <- constant 6 -< ()
+
+          five_plus_six <- plus -< (five, six)
+
+          returnA -< ((Just one, Nothing), Just five_plus_six)
         sqlInsert' = arrangeInsert def' def table insertExpr
         def' = unPPOfContravariant def
 
 testUpdate :: String
 testUpdate = show (ppUpdate sqlUpdate')
-  where table :: Table ((Wire Int, Wire Int), Wire Int)
-        table  = Table "tablename" ((Wire "col1", Wire "col2"), Wire "col3")
-        updateExpr :: ExprArr ((Wire Int, Wire Int),
+  where updateExpr :: ExprArr ((Wire Int, Wire Int),
                                Wire Int)
                               ((Maybe (Wire Int), Maybe (Wire Int)),
                                Maybe (Wire Int))
-        updateExpr = (arr (const Nothing) &&& (arr Just <<< plus <<< arr fst))
-                     &&& arr (const Nothing)
+        updateExpr = proc ((x, y), _) -> do
+          x_plus_y <- plus -< (x, y)
+          returnA -< ((Nothing, Just x_plus_y), Nothing)
         condExpr :: ExprArr ((Wire Int, Wire Int), Wire Int) (Wire Bool)
-        condExpr = eq <<< arr ((fst . fst) &&& snd)
-
+        condExpr = proc ((x, _), z) -> do
+          eq -< (x, z)
         sqlUpdate' = arrangeUpdate def def' def table updateExpr condExpr
         def' = unPPOfContravariant def

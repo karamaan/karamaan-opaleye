4dc006e39b3582ee7d9fe2d937285baf24358086
Tom Ellis <tom-2013@purelyagile.com>
2013-12-30 11:51:23 +0000

    Add fromNullable and fromNullable'

--- Karamaan/Opaleye/Nullable.hs
+++ Karamaan/Opaleye/Nullable.hs
@@ -1,17 +1,22 @@
 module Karamaan.Opaleye.Nullable where
 
-import Karamaan.Opaleye.QueryArr (QueryArr)
+import Karamaan.Opaleye.QueryArr (QueryArr, Query)
 import Karamaan.Opaleye.Wire (Wire)
 import qualified Karamaan.Opaleye.Operators2 as Op2
 import qualified Karamaan.Opaleye.Join as Join
-import Control.Arrow ((<<<))
+import Control.Arrow ((<<<), second)
 
 -- This is just used a phantom type in 'Wire's.
 -- It's not actually used for values.
 data Nullable a = PhantomNullable
 
--- In the ideal world we are creating, Op2.isNull goes away because it
--- uses the old Maybe type constructor, and this is implemented
--- directly.
+-- In the ideal world we are creating the 'Maybe' functions in Op2 go
+-- away and these are implemented directly.
 isNull :: QueryArr (Wire (Nullable a)) (Wire Bool)
 isNull = Join.unsafeCoerce <<< Op2.isNull <<< Join.unsafeCoerce
+
+fromNullable :: QueryArr (Wire a, Wire (Maybe a)) (Wire a)
+fromNullable = Op2.fromMaybe <<< second Join.unsafeCoerce
+
+fromNullable' :: Query (Wire a) -> QueryArr (Wire (Nullable a)) (Wire a)
+fromNullable' w = Op2.fromMaybe' w <<< Join.unsafeCoerce

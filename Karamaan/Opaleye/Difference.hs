f8d6643cc21caf0a8741f8909a2a645444a58617
Tom Ellis <tom-2013@purelyagile.com>
2014-03-17 20:47:02 +0000

    Add Difference.without

---
+++ Karamaan/Opaleye/Difference.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE Arrows #-}
+
+module Karamaan.Opaleye.Difference where
+
+import Karamaan.Opaleye.QueryArr (Query)
+import Karamaan.Opaleye.Wire (Wire)
+import qualified Karamaan.Opaleye.Distinct as D
+import qualified Karamaan.Opaleye.Operators2 as O
+import qualified Karamaan.Opaleye.Predicates as P
+import Control.Arrow ((<<<), returnA, arr)
+
+-- 'without column exclude q' returns all the rows of 'q' except those
+-- where the column of 'q' selected by 'column' occurs in 'exclude'
+without :: (b -> Wire a) -> Query (Wire a) -> Query b -> Query b
+without f exclude q = proc () -> do
+  keep <- D.distinct1 (arr f <<< q) `O.difference` exclude -< ()
+  row  <- q -< ()
+  P.restrict <<< O.eq -< (keep, f row)
+  returnA -< row

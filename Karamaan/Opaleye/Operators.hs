83a8ca6ecca2c881a633469921437e3a1790fd85
Tom Ellis <tom-2013@purelyagile.com>
2014-04-18 10:49:14 +0100

    Remove unused 'constant'

--- Karamaan/Opaleye/Operators.hs
+++ Karamaan/Opaleye/Operators.hs
@@ -1,8 +1,5 @@
 module Karamaan.Opaleye.Operators where
 
-import Database.HaskellDB.Query (ShowConstant, showConstant)
-import Database.HaskellDB.PrimQuery (PrimExpr(ConstExpr))
-
 -- TODO vv I put this take 5 in here because the query strings were getting
 -- too long and postgres was complaining that it was truncating them.
 -- This is really just a temporary fix, because I'd like to keep the
@@ -11,8 +8,3 @@ import Database.HaskellDB.PrimQuery (PrimExpr(ConstExpr))
 operatorName :: String -> String -> String -> String
 operatorName left opName right = concat [t left, "_", opName, "_", t right]
   where t = take 5
-
--- If you think you want to use this, you're probably better off with
--- Operators2.constant.  (Sorry for the confusing naming scheme).
-constant :: ShowConstant a => a -> PrimExpr
-constant = ConstExpr . showConstant

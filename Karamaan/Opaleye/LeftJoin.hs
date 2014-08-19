ae3c85e455999189594c57b9c0b1edbaaa117ef0
Tom Ellis <tom-2013@purelyagile.com>
2014-08-19 11:21:02 -0400

    Reorder code

--- Karamaan/Opaleye/LeftJoin.hs
+++ Karamaan/Opaleye/LeftJoin.hs
@@ -6,12 +6,6 @@ module Karamaan.Opaleye.LeftJoin where
 {-
 A hacky implementation of LEFT JOIN for Opaleye.
 
-    leftJoin' q1 q2 expr
-
-does the equivalent of
-
-    q1 LEFT OUTER JOIN q2 ON expr
-
 There is a better approach to implementing LEFT JOIN in Opaleye but it
 will be more time consuming:
 
@@ -42,71 +36,24 @@ import qualified Karamaan.Opaleye.SQL as SQL
 import qualified Karamaan.Opaleye.ExprArr as E
 import qualified Data.Profunctor.Product as PP
 
--- FIXME: this seems to fail on the union because the NULLs are not
--- given explicit types.  This is an annoyance of Postgres.  There'll be
--- a way to work around it (of course: just give the NULLs explicity types!)
--- but it is annoying.
+{-
+    leftJoin' q1 q2 expr
 
--- NullMaker a b represents a way of turning a 'QueryArr z a' into a
--- 'QueryArr z b' where all the columns of 'b' are made nullable.
--- For example 'QueryArr (Wire Int, Wire Bool, Wire String)' could
--- become 'QueryArr (Wire (Maybe Int), Wire (Maybe Bool), Wire (Maybe String)'.
---
--- I don't really like that this is 'a -> b'.  To be safe it should be
--- QueryArr a b, or ExprArr a b, when that exists.  I don't think it
--- will cause any problems though, if it is not exported.
---
--- We don't need the second component for the new join implementation.
--- It was only to "fill in nulls" in the old, bad, version.
--- We can delete this component when we delete the old, bad, version.
-data NullMaker a b = NullMaker (a -> b) (Query b)
+does the equivalent of
 
-toNullable :: NullMaker a b -> a -> b
-toNullable (NullMaker f _) = f
-
--- When we have proper support for ExprArr I suppose this can be
--- NullMaker a b -> Expr b
-nulls :: NullMaker a b -> Query b
-nulls (NullMaker _ n) = n
-
--- NB: We probably also need an instance
---
---       Default NullMaker (Wire (Maybe a)) (Wire (Maybe a))
-
-instance Default NullMaker (Wire a) (Wire (Maybe a)) where
-  def = NullMaker Wire.unsafeCoerce (Op2.constantLit PQ.NullLit)
-
-instance Profunctor NullMaker where
-  dimap f g nm = NullMaker (dimap f g (toNullable nm)) (fmap g (nulls nm))
-
-instance ProductProfunctor NullMaker where
-  empty = NullMaker id (arr id)
-  NullMaker f n ***! NullMaker f' n' = NullMaker (f *** f') (n &&& n')
-
-{-# DEPRECATED leftJoin "This was for exploratory purposes only" #-}
-leftJoin :: (Default QueryColspec l l, Default QueryColspec r' r')
-         => NullMaker r r'
-         -> Query l -> (l -> Wire b)
-         -> Query r -> (r -> Wire b)
-         -> Query (l, r')
-leftJoin nm qL fL qR fR = join `Op2.union` outer
-  where join = proc () -> do
-          rowL <- qL -< ()
-          keyL <- arr fL -< rowL
-
-          rowR <- qR -< ()
-          keyR <- arr fR -< rowR
-
-          P.restrict <<< Op2.eq -< (keyL, keyR)
-
-          returnA -< (rowL, toNullable nm rowR)
-
-        outer = proc () -> do
-          rowL <- qL `Op2.difference` (arr fst <<< join) -< ()
-
-          nulls' <- nulls nm -< ()
-
-          returnA -< (rowL, nulls')
+    q1 LEFT OUTER JOIN q2 ON expr
+-}
+-- Eventually this one will be renamed leftJoin, I suppose, but that's
+-- a breaking change.
+leftJoin' :: (Default (PP.PPOfContravariant U.Unpackspec) wiresA wiresA,
+              Default (PP.PPOfContravariant U.Unpackspec) wiresB wiresB,
+              Default NullMaker wiresB wireNullablesB)
+             => Query wiresA -> Query wiresB
+              -> E.ExprArr (wiresA, wiresB) (Wire Bool)
+              -> Query (wiresA, wireNullablesB)
+leftJoin' = leftJoinPP (PP.unPPOfContravariant def)
+                       (PP.unPPOfContravariant def)
+                       def
 
 -- This is implemented using the trick of shimming arbitrary SQL into
 -- a PrimQuery.  We use the BaseTable constructor and give it the SQL
@@ -143,12 +90,69 @@ leftJoinPP unpackA unpackB nullmaker qA qB expr = Q.simpleQueryArr f where
             -- NB ^^ these are the same as in Table.hs
           wireNullablesB = toNullable nullmaker wiresB
 
-leftJoin' :: (Default (PP.PPOfContravariant U.Unpackspec) wiresA wiresA,
-              Default (PP.PPOfContravariant U.Unpackspec) wiresB wiresB,
-              Default NullMaker wiresB wireNullablesB)
-             => Query wiresA -> Query wiresB
-              -> E.ExprArr (wiresA, wiresB) (Wire Bool)
-              -> Query (wiresA, wireNullablesB)
-leftJoin' = leftJoinPP (PP.unPPOfContravariant def)
-                       (PP.unPPOfContravariant def)
-                       def
+-- FIXME: this seems to fail on the union because the NULLs are not
+-- given explicit types.  This is an annoyance of Postgres.  There'll be
+-- a way to work around it (of course: just give the NULLs explicity types!)
+-- but it is annoying.
+
+-- NullMaker a b represents a way of turning a 'QueryArr z a' into a
+-- 'QueryArr z b' where all the columns of 'b' are made nullable.
+-- For example 'QueryArr (Wire Int, Wire Bool, Wire String)' could
+-- become 'QueryArr (Wire (Maybe Int), Wire (Maybe Bool), Wire (Maybe String)'.
+--
+-- I don't really like that this is 'a -> b'.  To be safe it should be
+-- QueryArr a b, or ExprArr a b, when that exists.  I don't think it
+-- will cause any problems though, if it is not exported.
+--
+-- We don't need the second component for the new join implementation.
+-- It was only to "fill in nulls" in the old, bad, version.
+-- We can delete this component when we delete the old, bad, version.
+data NullMaker a b = NullMaker (a -> b) (Query b)
+
+toNullable :: NullMaker a b -> a -> b
+toNullable (NullMaker f _) = f
+
+-- When we have proper support for ExprArr I suppose this can be
+-- NullMaker a b -> Expr b
+nulls :: NullMaker a b -> Query b
+nulls (NullMaker _ n) = n
+
+-- NB: We probably also need an instance
+--
+--       Default NullMaker (Wire (Maybe a)) (Wire (Maybe a))
+instance Default NullMaker (Wire a) (Wire (Maybe a)) where
+  def = NullMaker Wire.unsafeCoerce (Op2.constantLit PQ.NullLit)
+
+instance Profunctor NullMaker where
+  dimap f g nm = NullMaker (dimap f g (toNullable nm)) (fmap g (nulls nm))
+
+instance ProductProfunctor NullMaker where
+  empty = NullMaker id (arr id)
+  NullMaker f n ***! NullMaker f' n' = NullMaker (f *** f') (n &&& n')
+
+--
+
+{-# DEPRECATED leftJoin "This was for exploratory purposes only" #-}
+leftJoin :: (Default QueryColspec l l, Default QueryColspec r' r')
+         => NullMaker r r'
+         -> Query l -> (l -> Wire b)
+         -> Query r -> (r -> Wire b)
+         -> Query (l, r')
+leftJoin nm qL fL qR fR = join `Op2.union` outer
+  where join = proc () -> do
+          rowL <- qL -< ()
+          keyL <- arr fL -< rowL
+
+          rowR <- qR -< ()
+          keyR <- arr fR -< rowR
+
+          P.restrict <<< Op2.eq -< (keyL, keyR)
+
+          returnA -< (rowL, toNullable nm rowR)
+
+        outer = proc () -> do
+          rowL <- qL `Op2.difference` (arr fst <<< join) -< ()
+
+          nulls' <- nulls nm -< ()
+
+          returnA -< (rowL, nulls')
